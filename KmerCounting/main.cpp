b3213a91b3dfa9187344b79bb028f3bd9ff1e231
MehtaKnight01 <rishil.a.mehta@gmail.com>
2016-06-02 15:27:46 -0500

    Initial Commit

---
+++ KmerCounting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KmerCounting
+//
+//  Created by Rishil on 6/2/16.
+//  Copyright © 2016 Rishil Mehta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
